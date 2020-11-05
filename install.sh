3c4c8d663cc678159adfeb9a46ec55244864c19e
user <user@debian.local>
2020-11-05 13:10:36 +0100

    modification de install.sh pour télécharger l'iso de vyos

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 apt-get install -y squashfs-tools docker-compose wget
 mkdir ./vyos/unsquashfs
 mkdir ./vyos/rootfs
+wget -c https://downloads.vyos.io/rolling/current/amd64/vyos-rolling-latest.iso -O ./vyos/vyos-latest.iso
 mount -o loop ./vyos/vyos-latest.iso ./vyos/rootfs
 unsquashfs -f -d ./vyos/unsquashfs/ ./vyos/rootfs/live/filesystem.squashfs
 tar -C ./vyos/unsquashfs -c . | docker import - vyos
