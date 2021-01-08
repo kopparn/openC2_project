992535341c78902144349c845a2dee10bdd04ee3
user <user@debian.local>
2021-01-08 12:08:57 +0100

    Correction du code du proxy mysql

--- src/mysql-proxy/main.py
+++ src/mysql-proxy/main.py
@@ -2,23 +2,23 @@ import stix2
 import http.client
 import sys
 import os
-sys.path.append("..")
+sys.path.append("/etc/openc2-proxy")
 import openc2
 import json
 
 def main():
-    ms = sys.stdin.replace("\'","\"")
+    ms = sys.stdin
     msg = json.load(ms)
     openc2_cmd = openc2.parse(msg)
     openc2_cmd.check_object_constraints()
     action = openc2_cmd["action"]
     db_name = msg["target"]["db:db_name"] 
     if action == "copy" :
-        mysql_cmd = "mysqldump -u root --password=toor --databases " + db_name + " > " + db_name + ".sql"
+        mysql_cmd = "mysqldump -u root --password=toor --databases " + db_name + " > " + "/var/www/backups/" + db_name + ".sql"
     elif action == "restore" :
-        mysql_cmd = "mysql -u root --password=toor < " + db_name + ".sql"
+        mysql_cmd = "mysql -u root --password=toor < " + "/var/www/backups/" + db_name + ".sql"
     elif action == "delete" :
-        mysql_cmd = "rm " + db_name + ".sql"
+        mysql_cmd = "rm " + "/var/www/backups/" + db_name + ".sql"
     os.system(mysql_cmd)
 
 if __name__ == '__main__':
