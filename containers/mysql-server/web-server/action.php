5c581ea40abaa34b5fbdb1ebf3136e8c48640c2f
kopparn <arnaud.kopp@telecom-sudparis.eu>
2021-01-31 23:38:23 +0100

    Correction d'un bug sur la page d'authentification

--- containers/mysql-server/web-server/action.php
+++ containers/mysql-server/web-server/action.php
@@ -3,11 +3,19 @@ $login = $_POST['login'];
 $passwd = $_POST['passwd'];
 $mysqli = mysqli_connect("localhost", "root", "toor", "my_database");
 $query = sprintf("SELECT name FROM Users WHERE name='%s' AND passwd='%s'",$login,$passwd);
-$res = mysqli_multi_query($mysqli, $query);
-$row = mysqli_fetch_assoc($res);
-if ($row['name']) {
-	echo "Bienvenue ", $row['name'];
-} else {
-	echo "Echec de la connexion";
+if ($mysqli->multi_query($query)) {
+    do {
+        if ($result = $mysqli->store_result()) {
+            while ($row = $result->fetch_row()) {
+                if ($row['name']) {
+			echo "Bienvenue ", $row['name'];
+		} else {
+			echo "Echec de la connexion";		
+		}
+            }
+            $result->free();
+        }
+    } while ($mysqli->next_result());
 }
+$mysqli->close();
 ?>
